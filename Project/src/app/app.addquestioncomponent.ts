c49d74527e09ffd101e337b49fad6670a4e8338a
Admin <Admin@DESKTOP-2MHT97K>
2019-10-19 17:19:53 +0530

    modified question

--- Project/src/app/app.addquestioncomponent.ts
+++ Project/src/app/app.addquestioncomponent.ts
@@ -1,7 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
     selector:'addquestion',
     templateUrl:'app.addquestion.html'
 })
-export class AddQuestion{ }
+export class AddQuestion implements OnInit{ 
+    buttonVal:boolean=false;
+    questionForm:FormGroup;
+
+    changeButtonValue(value:boolean){
+        if(value==true){
+            this.buttonVal=false;
+        }
+        else{
+            this.buttonVal=true;
+        }
+    }
+
+    constructor(private fb:FormBuilder){}
+
+    ngOnInit(){
+        this.questionForm = this.fb.group({
+            testid:['',Validators.required]
+        })
+    }
+}
