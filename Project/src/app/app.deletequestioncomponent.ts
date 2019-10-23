48c6ed9e3fd3fbe9e63a5b3503ec75c893e2fee6
swanand <pandeswanand@gmail.com>
2019-10-23 23:46:04 +0530

    added modifications

--- Project/src/app/app.deletequestioncomponent.ts
+++ Project/src/app/app.deletequestioncomponent.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Question } from "./_model/app.question";
 import { QuestionService } from "./_service/app.questionservice";
 import { Router } from "@angular/router";
@@ -7,13 +7,29 @@ import { Router } from "@angular/router";
     selector:'deletequestion',
     templateUrl:'app.deletequestion.html'
 })
-export class DeleteQuestion implements OnInit{
+export class DeleteQuestion {
 
+    questionId:number;
     constructor(private service:QuestionService, private router:Router){}
 
-    ngOnInit(){}
+    public popoverTitle:string="Delete Confirmation";
+    public popoverMessage:string="Do you really want to delete the question with question Id ";
 
-    deleteQuestion(id:number){
-        this.service.deleteQuestion(id).subscribe((success:string)=>{alert(success);this.router.navigate(['/admin']);}, error=>{alert(error.error);});
+    error_id:string=null;
+    validateQuestionId():boolean{
+        if(this.questionId != null){
+            this.error_id = null;
+            return true;
+        }
+        else{
+            this.error_id = "Question Id field cannot be empty!";
+            return false;
+        }
+    }
+
+    deleteQuestion(){
+        if(this.validateQuestionId()){
+            this.service.deleteQuestion(this.questionId).subscribe((success:string)=>{alert(success);this.router.navigate(['/admin']);}, error=>{alert(error.error);});
+        }
     }
 }
